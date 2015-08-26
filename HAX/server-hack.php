357e9aebb829045b3002a0860fb8d3ecde41a623
ArcanaMagus <ghost1@users.noreply.github.com>
2015-08-26 13:28:49 -0400

    Added hacked support for PHP's built in $_SERVER['REQUEST_URI'] variance

---
+++ HAX/server-hack.php
@@ -0,0 +1,13 @@
+function check_server_request_uri()
+{
+		if (!isset($_SERVER['REQUEST_URI']))
+		{
+		$_SERVER['REQUEST_URI'] =$_SERVER['PHP_SELF'];
+		
+						// Sometimes, web server set QUERY_STRING but is empty.
+					if (isset($_SERVER['QUERY_STRING']) AND $ $_SERVER['QUERY_STRING'] != "")
+					{
+					$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
+		}
+		}	
+}		
