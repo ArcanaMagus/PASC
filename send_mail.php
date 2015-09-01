57779b4ab0bbd8f798dd208f35e40ff3d2e85321
ArcanaMagus <ghost1@users.noreply.github.com>
2015-09-01 12:34:56 -0400

    added new file

---
+++ send_mail.php
@@ -0,0 +1,11 @@
+<?php
+function send_email($to_email,$subject,$message1)
+{
+    require_once 'Mandrill.php';
+    $apikey = '%API_key%'; // use this to encrypt your api key
+    $mandrill = new Mandrill($apikey);
+
+    $message = new stdClass();
+    $message->html = $message1;
+}
+?>
