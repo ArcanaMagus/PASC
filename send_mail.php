0d8f37ac698bff26d0826b09c8cfc2493121341c
ArcanaMagus <ghost1@users.noreply.github.com>
2015-09-01 12:36:35 -0400

    modified file with whitespace

--- send_mail.php
+++ send_mail.php
@@ -2,10 +2,10 @@
 function send_email($to_email,$subject,$message1)
 {
     require_once 'Mandrill.php';
-    $apikey = '%API_key%'; // use this to encrypt your api key
-    $mandrill = new Mandrill($apikey);
+$apikey = '%API_key%'; // use this to encrypt your api key
+$mandrill = new Mandrill($apikey);
 
-    $message = new stdClass();
-    $message->html = $message1;
+$message = new stdClass();
+$message->html = $message1;
 }
 ?>
