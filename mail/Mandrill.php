2e81e5b133f1dc11dbcb5f6acba502337e1b1b0b
ArcanaMagus <ghost1@users.noreply.github.com>
2015-09-02 16:18:14 -0400

    Add Mandrill dependency file

--- mail/Mandrill.php
+++ mail/Mandrill.php
@@ -1,4 +1,11 @@
 <?php
-
+/**
+  * The following script contains volumes for comsumption
+  * Mandril Services
+  *  ~>package send_mail
+  *  
+  *  ~> author anonymous <>
+  *  
+  *  ~> version 1.0 
 
 ?>
